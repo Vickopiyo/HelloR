53a853bd145c4e48491b018f565f1d1a3ff6ff73
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-27 12:16:55 +0300

    Fix body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,8 +5,11 @@ const Body = () => {
     <div>
         <h2>About Me</h2>
         <p>I am a passionate and self driven developer who looks to immpact lives around the globe through developing softwares that solve problems around the world.</p>
-        <h2>SKills</h2>
-        <p>I well versed inn HTML,CSS and Javascript.I"m currently learning React</p>
+        <h2>Skills</h2>
+        <p>I well versed inn HTML,CSS and Javascript.I"m currently learning React.</p>
+
+        <h2>Projects</h2>
+        <p>I have quite a number of projects and this one is the latest one.</p>
         
     </div>
   )
