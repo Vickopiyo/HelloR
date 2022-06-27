20e0acfe7077f2731200d88b8df38ec60f57346e
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-27 09:44:57 +0300

    My first R app

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <h2>All  rights reserved 2022</h2>
+
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Hello my name is Victor</h1>
+        <h2>I'm learning React</h2>
+        
+    </header>
+  )
+}
+
+export default Header
